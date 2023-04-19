11795dbb6b875c7083b7e9ff166522578583ad4a
odhjustingabriel <113585930+odhjustingabriel@users.noreply.github.com>
2023-04-19 04:07:54 +0300

    Create bmi-calc.c

---
+++ bmi-calc.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+    float weight, height, bmi;
+    char* status;
+
+    printf("Enter your weight in kilograms: ");
+    scanf("%f", &weight);
+
+    printf("Enter your height in meters: ");
+    scanf("%f", &height);
+
+    bmi = weight / (height * height);
+
+    if (bmi < 18.5) {
+        status = "underweight";
+    } else if (bmi >= 18.5 && bmi < 25) {
+        status = "healthy";
+    } else if (bmi >= 25 && bmi < 30) {
+        status = "overweight";
+    } else {
+        status = "obese";
+    }
+
+    printf("Your BMI is %.2f, which is %s\n", bmi, status);
+
+    return 0;
+}
